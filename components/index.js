690dd0b7b462925f72b485e0aada0b4a09f98192
cuiyq075145 <cuiyq075145@yimidida.com>
2020-04-14 21:36:49 +0800

    111

--- components/index.js
+++ components/index.js
@@ -2,7 +2,7 @@ function changeStr(str) {
   // 首字母大写
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
-// 找到上一级common目录下的.vue结尾的所有文件
+  // 找到上一级common目录下的.vue结尾的所有文件
 const requireComponent = require.context('@/components', false, /\.vue$/);
 const install = (Vue) => {
   requireComponent.keys().forEach((fileName) => {
